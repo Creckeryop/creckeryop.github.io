6aca58d96adfff4e0f017845e7fd68e1b2e76514
Creckeryop <didager@yandex.ru>
2020-03-02 16:35:19 +0500

    add anchor

--- js/index.js
+++ js/index.js
@@ -52,3 +52,13 @@ function loadProjectsInfo() {
 }
 
 loadProjectsInfo();
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
